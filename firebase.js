7abd9aa61a2373c5180e4e156a4e929b0e4adc7e
kkstockanalysis-arch <kkstockanalysis@gmail.com>
2026-01-27 20:41:39 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCHxK7Zg-JJB4fYiXe2J_Z-wcLeTIdDm1c",
+  authDomain: "kk-stock-analysis-d7b43.firebaseapp.com",
+  projectId: "kk-stock-analysis-d7b43",
+  storageBucket: "kk-stock-analysis-d7b43.firebasestorage.app",
+  messagingSenderId: "338926544975",
+  appId: "1:338926544975:web:f1598c672a002f706ca23a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
