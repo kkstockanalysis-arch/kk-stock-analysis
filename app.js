f5770b141c3e225b4a0c66b97a75cd67dd78df37
kkstockanalysis-arch <kkstockanalysis@gmail.com>
2026-01-28 21:27:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,22 +4,29 @@ const signupPage = document.getElementById("signupPage");
 const dashboardPage = document.getElementById("dashboardPage");
 
 // ===== SHOW FUNCTIONS =====
+// ===== LOGIN =====
 function showLogin() {
   loginPage.classList.remove("hidden");
   signupPage.classList.add("hidden");
   dashboardPage.classList.add("hidden");
 }
 
+// ===== SIGN UP =====
 function showSignup() {
   loginPage.classList.add("hidden");
   signupPage.classList.remove("hidden");
   dashboardPage.classList.add("hidden");
 }
 
+
+// ===== SHOW DASH BOARD =====
 function showDashboard() {
   loginPage.classList.add("hidden");
   signupPage.classList.add("hidden");
   dashboardPage.classList.remove("hidden");
+
+  loadStockPrice();
+
 }
 
 // ===== BUTTON ACTIONS =====
